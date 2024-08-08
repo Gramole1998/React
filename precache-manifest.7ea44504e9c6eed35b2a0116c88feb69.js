d7c6d0e6514f5644dfd4656d894f0ac300d2a529
Gaurav <ramolegaurav9@gmail.com>
2024-08-08 21:25:47 +0530

    Updates

---
+++ precache-manifest.7ea44504e9c6eed35b2a0116c88feb69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315240c62629f8107c3a7cf51a709f65",
+    "url": "/React.git/index.html"
+  },
+  {
+    "revision": "aca2bfc3ed982cd8deac",
+    "url": "/React.git/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "4a6bee31c838c584c680",
+    "url": "/React.git/static/css/main.597fcbed.chunk.css"
+  },
+  {
+    "revision": "aca2bfc3ed982cd8deac",
+    "url": "/React.git/static/js/2.17f92a83.chunk.js"
+  },
+  {
+    "revision": "5b4d715d19f254b12429443a305745a5",
+    "url": "/React.git/static/js/2.17f92a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7427fe9bee336e9698de",
+    "url": "/React.git/static/js/3.4776b16b.chunk.js"
+  },
+  {
+    "revision": "4a6bee31c838c584c680",
+    "url": "/React.git/static/js/main.a2052dd7.chunk.js"
+  },
+  {
+    "revision": "0ddae2fdf1603b4f844b",
+    "url": "/React.git/static/js/runtime-main.ef53d14b.js"
+  }
+]);
